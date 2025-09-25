b08afbdd64f94378a7947dabac4c720be6fa79cb
Ramjee194 <ramjeekumaryadav733@gmail.com>
2025-09-25 23:54:00 +0530

    jo

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,16 +4,18 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  
   plugins: [
     react(),
     tailwindcss(),
   ],
   server: {
     proxy: {
-   
-       '/api': 'https://lms-1-ki76.onrender.com',
+      '/api': 'https://lms-1-ki76.onrender.com',
     },
-  }
-  
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 5173,
+    allowedHosts: ["onelms-uode.onrender.com"], // add your Render domain
+  },
 })
